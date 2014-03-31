5541523aa15999358566e7a0b66dae24fb5c5e61
sam walton <sam@foofrancine.com>
2014-03-31 09:27:18 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hstorey::Application.config.secret_key_base = 'c8564e9e5f278a35c8665b8e0a65cd37a6d404587286bba0aa845d720cf51cb088ffab0c907d1c1116639954d63f9e687e2c00bbd0673f4a74f721f71a72891a'
